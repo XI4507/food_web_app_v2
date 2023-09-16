15badb6823063a70d08143881f78c3d86a6fd7fa
XI4507 <rakeshsingh9625@gmail.com>
2023-09-16 12:18:34 +0530

    day2 of project copy data from swiggy api

--- App.js
+++ App.js
@@ -1,6 +1,595 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const restaurants=[
+    {
+      "info": {
+        "id": "326440",
+        "name": "Punjabi Angithi",
+        "cloudinaryImageId": "g2pklvjnapzfoc4ackou",
+        "locality": "Paschim Vihar",
+        "areaName": "Paschim Vihar",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "North Indian",
+          "Chinese",
+          "Punjabi",
+          "Kebabs",
+          "Tandoor"
+        ],
+        "avgRating": 4.1,
+        "feeDetails": {
+          "restaurantId": "326440",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 5300
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 5300
+        },
+        "parentId": "4464",
+        "avgRatingString": "4.1",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 34,
+          "lastMileTravel": 5.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "34 mins",
+          "lastMileTravelString": "5.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-09-16 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          "textExtendedBadges": [
+            {
+              "iconId": "guiltfree/GF_Logo_android_3x",
+              "shortDescription": "options available",
+              "fontColor": "#7E808C"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "",
+                    "fontColor": "#7E808C",
+                    "iconId": "guiltfree/GF_Logo_android_3x",
+                    "shortDescription": "options available"
+                  }
+                }
+              ]
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹125 OFF",
+          "subHeader": "ABOVE ₹199",
+          "discountTag": "FLAT DEAL"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/punjabi-angithi-paschim-vihar-delhi-326440",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "27025",
+        "name": "KFC",
+        "cloudinaryImageId": "56c9ab92bd79745fd152a30fa2525426",
+        "locality": "Unity One Mall",
+        "areaName": "Rohini",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Burgers",
+          "Biryani",
+          "American",
+          "Snacks",
+          "Fast Food"
+        ],
+        "avgRating": 4.1,
+        "feeDetails": {
+          "restaurantId": "27025",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 4100
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 4100
+        },
+        "parentId": "547",
+        "avgRatingString": "4.1",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 20,
+          "lastMileTravel": 2.1,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20 mins",
+          "lastMileTravelString": "2.1 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-09-16 23:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "20% OFF",
+          "subHeader": "UPTO ₹50"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/kfc-unity-one-mall-rohini-delhi-27025",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "253731",
+        "name": "McDonald's",
+        "cloudinaryImageId": "6dc3ed2ca21d71acff7c2a51dfe4c720",
+        "locality": "Sector 3",
+        "areaName": "Rohini",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "American"
+        ],
+        "avgRating": 4.3,
+        "feeDetails": {
+          "restaurantId": "253731",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3600
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 3600
+        },
+        "parentId": "630",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "5K+",
+        "sla": {
+          "deliveryTime": 24,
+          "lastMileTravel": 2,
+          "serviceability": "SERVICEABLE",
+          "slaString": "24 mins",
+          "lastMileTravelString": "2.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-09-16 23:45:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "aggregatedDiscountInfoV2": {
+          
+        },
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/mcdonalds-sector-3-rohini-delhi-253731",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "10208",
+        "name": "Burger King",
+        "cloudinaryImageId": "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+        "locality": "Sector 10",
+        "areaName": "Rohini",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "avgRating": 4.2,
+        "feeDetails": {
+          "restaurantId": "10208",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3600
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 3600
+        },
+        "parentId": "166",
+        "avgRatingString": "4.2",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 20,
+          "lastMileTravel": 2,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20 mins",
+          "lastMileTravelString": "2.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-09-17 03:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "60% OFF",
+          "subHeader": "UPTO ₹120"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/burger-king-sector-10-rohini-delhi-10208",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "341033",
+        "name": "Bakingo",
+        "cloudinaryImageId": "rmnzl74nmlldrsv9bnvk",
+        "locality": "Rithala",
+        "areaName": "Rohini",
+        "costForTwo": "₹300 for two",
+        "cuisines": [
+          "Bakery",
+          "Desserts",
+          "Beverages",
+          "Snacks"
+        ],
+        "avgRating": 4.3,
+        "feeDetails": {
+          "restaurantId": "341033",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 4400
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 4400
+        },
+        "parentId": "3818",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "1K+",
+        "sla": {
+          "deliveryTime": 22,
+          "lastMileTravel": 3,
+          "serviceability": "SERVICEABLE",
+          "slaString": "22 mins",
+          "lastMileTravelString": "3.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-09-17 00:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "aggregatedDiscountInfoV2": {
+          
+        },
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/bakingo-rithala-rohini-delhi-341033",
+        "type": "WEBLINK"
+      }
+    },
+    {
+      "info": {
+        "id": "10392",
+        "name": "Subway",
+        "cloudinaryImageId": "1ace5fa65eff3e1223feb696c956b38b",
+        "locality": "Sector 3",
+        "areaName": "Rohini",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Salads",
+          "Snacks",
+          "Desserts",
+          "Beverages"
+        ],
+        "avgRating": 4,
+        "feeDetails": {
+          "restaurantId": "10392",
+          "fees": [
+            {
+              "name": "BASE_DISTANCE",
+              "fee": 3600
+            },
+            {
+              "name": "BASE_TIME"
+            },
+            {
+              "name": "ANCILLARY_SURGE_FEE"
+            }
+          ],
+          "totalFee": 3600
+        },
+        "parentId": "2",
+        "avgRatingString": "4.0",
+        "totalRatingsString": "10K+",
+        "sla": {
+          "deliveryTime": 20,
+          "lastMileTravel": 2,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20 mins",
+          "lastMileTravelString": "2.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2023-09-17 04:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "40% OFF",
+          "subHeader": "UPTO ₹80"
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        }
+      },
+      "analytics": {
+        
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/restaurants/subway-sector-3-rohini-delhi-10392",
+        "type": "WEBLINK"
+      }
+    }
+]
 const Header = () => {
   return (
     <div className="header">
@@ -23,12 +612,13 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = ({restaurant}) => {
+  const {name,cuisines}=restaurant.info;
   return (
     <div className="restaurant-card">
       <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/56c9ab92bd79745fd152a30fa2525426" alt="res-card" />
-      <h3>KFC</h3>
-      <p>Fries , Burger ,Wings</p>
+      <h3>{name}</h3>
+      <p>{cuisines.join(",")}</p>
       <p>2.3 km</p>
     </div>
   );
@@ -39,7 +629,7 @@ const Body = () => {
     <div>
       <div className="search-bar">seacrh</div>
       <div className="restaurant-container">
-        <RestaurantCard />
+        <RestaurantCard restaurant={restaurants[0]} />
       </div>
     </div>
   );
