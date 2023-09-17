f6bc3aedde7c8041f551227e2f1cd0be29e832ad
XI4507 <rakeshsingh9625@gmail.com>
2023-09-17 11:52:01 +0530

    created filter button top rated and all restaurant

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../utils/constants";
+import { useState } from "react";
 
 const Body = () => {
+    const [allRestaurant,setAllRestaurant]=useState(restaurants);
+    const [restaurant,setRestaurant]=useState(restaurants)
+    function handleTopRated(){
+        const filterRestaurant=allRestaurant.filter((res)=>res.info.avgRating>4.1);
+        setRestaurant(filterRestaurant)
+    }
     return (
       <div>
-        <div className="search-bar">seacrh</div>
+        <div className="filter-buttons">
+            <button className="btn" onClick={()=>handleTopRated()}>Top Rated</button>
+            <button className="btn" onClick={()=>setRestaurant(allRestaurant)}>All Restaurant</button>
+        </div>
         <div className="restaurant-container">
           {
-              restaurants.map((res)=><RestaurantCard restaurant={res} key={res.info.id}/>)
+              restaurant.map((res)=><RestaurantCard restaurant={res} key={res.info.id}/>)
           }
         </div>
       </div>
