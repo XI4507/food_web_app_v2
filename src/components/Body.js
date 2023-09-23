a30c890018c8570f3de75f8d9a130629762d9625
XI4507 <rakeshsingh9625@gmail.com>
2023-09-23 11:04:02 +0530

    fetch  data of restaurant from live API

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurants } from "../utils/constants";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SWIGGY_API } from "../utils/constants";
 
 const Body = () => {
     const [allRestaurant,setAllRestaurant]=useState(restaurants);
     const [restaurant,setRestaurant]=useState(restaurants)
+    useEffect(()=>{
+       fetchData()
+    },[])
+
+    async function fetchData  (){
+        const data=await fetch(SWIGGY_API)
+        const json=await data.json();
+        const arrayOfCards=json.data.cards;
+        const restaurant_list="restaurant_grid_listing";
+        for (const cardObj of arrayOfCards) {
+          if (cardObj.card.card && cardObj.card.card.id === restaurant_list) {
+            const resData =
+            cardObj.card?.card?.gridElements?.infoWithStyle?.restaurants;
+            console.log(resData);
+            setAllRestaurant(resData);
+            setRestaurant(resData)
+          }
+        }
+    }
+    
     function handleTopRated(){
         const filterRestaurant=allRestaurant.filter((res)=>res.info.avgRating>4.1);
         setRestaurant(filterRestaurant)
---
+++ src/components/Shimmer.js
@@ -0,0 +1,10 @@
+
+const Shimmer=()=>{
+    return(
+        <div>
+            <h1>Shimmer UI loading ....</h1>
+        </div>
+    )
+}
+
+export default Shimmer;
